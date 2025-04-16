cbb69136d6a709bcdbac8b2e69a30d1a4a0742b7
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-16 18:22:37 +0200

    abstact

---
+++ SensiveProject.DataAccesLayer/Abstract/ICommentDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveProject.DataAccesLayer.Abstract
+{
+    public class ICommentDal : IGenericDal<Comment>
+    {
+        
+    {
+    
+}
---
+++ SensiveProject.DataAccesLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveProject.DataAccesLayer.Abstract
+{
+    public class IGenericDal<T> where T : class
+    {
+
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        List<T> GetAll();
+        T GetById(int id);
+
+    }
+
+}
