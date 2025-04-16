0b82a377a32f1571b1ceb2c6ef2d61001f7aecf4
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-16 18:56:07 +0200

    GenericRepository added

---
+++ SensiveProject.DataAccesLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using SensiveProject.DataAccesLayer.Abstract;
+using SensiveProject.DataAccesLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveProject.DataAccesLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly SensiveContext _context;
+
+        public GenericRepository(SensiveContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
