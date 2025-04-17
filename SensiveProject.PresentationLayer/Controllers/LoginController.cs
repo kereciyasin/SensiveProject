9b77a1adbcc67a954b565399f766a448b18745ee
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-17 12:05:19 +0200

    LoginController update

---
+++ SensiveProject.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SensiveProject.EntityLayer.Concrete;
+using SensiveProject.PresentationLayer.Models;
+
+namespace SensiveProject.PresentationLayer.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _singInManager;
+
+        public LoginController(SignInManager<AppUser> singInManager)
+        {
+            _singInManager = singInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            var result = await _singInManager.PasswordSignInAsync(model.Username, model.Password, true, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                return View();
+
+            }
+
+        }
+    }
+}
