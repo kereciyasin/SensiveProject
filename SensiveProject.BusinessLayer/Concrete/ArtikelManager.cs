b783e580fafd0e6d87a00a3b195ab0cb1920a38d
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-21 16:35:48 +0200

    ICategoryDal update

---
+++ SensiveProject.BusinessLayer/Concrete/ArtikelManager.cs
@@ -0,0 +1,38 @@
+﻿using SensiveProject.BusinessLayer.Abstract;
+using SensiveProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveProject.BusinessLayer.Concrete
+{
+    public class ArtikelManager : IArtikelService
+    {
+        public List<Artikel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Artikel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Artikel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Artikel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SensiveProject.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using SensiveProject.BusinessLayer.Abstract;
+using SensiveProject.DataAccesLayer.Abstract;
+using SensiveProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveProject.BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
